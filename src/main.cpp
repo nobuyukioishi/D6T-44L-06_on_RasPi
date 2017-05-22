af842fc63097fec1cc3c6b00b3cb721c3913cc3b
Your Name <b.b.bluescratch@gmail.com>
2017-05-22 19:38:35 +0900

    created Test.csv and modified some

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,12 @@ openFrameWorkでアプリを起動するところ
 */
 
 #include <cstdio>
-#include<iostream>
-#include<fstream>
-
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sys/time.h>
 #include "i2c_d6t.h"
+
 
 int main()
 {
@@ -19,21 +21,30 @@ int main()
 	
 	sensorPtr = new D6T(i2c_bus, address, iType);
 	
-	std:ofstream ofs("Test.csv"); //ファイル出力ストリーム
-
+	struct timeval now;
+	gettimeofday(&now, NULL);
+	struct tm *pnow = localtime(&now.tv_sec);
+	// print current time including millisecond
+	/* std::cout << pnow->tm_year+1900 << "/" << pnow->tm_mon + 1 << "/" << pnow->tm_mday
+		<< " " << pnow->tm_hour << ":" <<pnow->tm_min << ":" << pnow->tm_sec << "." << now.tv_usec; */
+	std::ofstream ofs("Test.csv"); //ファイル出力ストリーム
+	
 	int16_t *measurements = 0;
 	while(1) {
 		measurements = sensorPtr->measure();
-		
+		gettimeofday(&now, NULL);
+		pnow = localtime(&now.tv_sec);
+		ofs << pnow->tm_year+1900 << "/" << pnow->tm_mon + 1 << "/" << pnow->tm_mday
+		<< " " << pnow->tm_hour << ":" <<pnow->tm_min << ":" << pnow->tm_sec << "." << now.tv_usec << "," <<std::flush;
 		float temp_cel[16];
 		for(int i = 0; i<15; i++) {
 			temp_cel[i] = (float) measurements[i+1]*0.1f;
 			printf("%.1f,", temp_cel[i]);
-			std:ofs<<temp_cel[i]<<std:endl;
+			ofs<< std::fixed << std::setprecision(1) << temp_cel[i]<< "," <<std::flush;
 		}	
 		temp_cel[15] = (float) measurements[16]*0.1f;
 		printf("%.1f\n", temp_cel[15]);
-		ofs<<temp_celp[15]<<endl;	
+		ofs<< std::fixed << std::setprecision(1) << temp_cel[15]<<std::endl;	
 		sleep(1);
 	}
 	return 0;
